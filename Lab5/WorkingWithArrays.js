2c981ea4334baaeee1e7f2cc9911415009566538
Ryan Witteck <witteck.r@northeastern.edu>
2024-11-17 17:05:28 -0500

    4.5

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -15,7 +15,11 @@ export default function WorkingWithArrays(app) {
     }
     res.json(todos);
   });
-
+  app.post("/lab5/todos", (req, res) => {
+    const newTodo = { ...req.body,  id: new Date().getTime() };
+    todos.push(newTodo);
+    res.json(newTodo);
+  });
   app.get("/lab5/todos/create", (req, res) => {
     const newTodo = {
       id: new Date().getTime(),
@@ -54,6 +58,32 @@ export default function WorkingWithArrays(app) {
     const todo = todos.find((t) => t.id === parseInt(id));
     todo.description = description;
     res.json(todos);
+  });
+  app.delete("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+        res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
+        return;
+      }  
+    todos.splice(todoIndex, 1);
+    res.sendStatus(200);
+  });
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+    if (todoIndex === -1) {
+      res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
+      return;
+    }
+
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
   });
 
 }
