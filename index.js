401da0e1d93482ffb6a2113719e45a5745a6e799
Ryan Witteck <witteck.r@northeastern.edu>
2024-11-19 15:21:00 -0500

    fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: process.env.NETLIFY_URL || "https://kanbas-react-web-app-ryanwitteck.netlify.app"
   })
 );
 const sessionOptions = {
