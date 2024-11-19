099d69b17f8e2dc2bbdbff4601833d443aded741
Ryan Witteck <witteck.r@northeastern.edu>
2024-11-19 15:10:37 -0500

    users fix

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -3,10 +3,6 @@ import * as courseDao from "../Courses/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function UserRoutes(app) {
-  const createUser = (req, res) => {};
-  const deleteUser = (req, res) => {};
-  const findAllUsers = (req, res) => {};
-  const findUserById = (req, res) => {};
   const updateUser = (req, res) => {
     const userId = req.params.userId;
     const userUpdates = req.body;
@@ -29,10 +25,10 @@ export default function UserRoutes(app) {
     const { username, password } = req.body;
     const currentUser = dao.findUserByCredentials(username, password);
     if (currentUser) {
-      req.session["currentUser"] = currentUser;
-      res.json(currentUser);
+    req.session["currentUser"] = currentUser;
+    res.json(currentUser);
     } else {
-      res.status(401).json({ message: "Unable to login. Try again later." });
+    res.status(401).json({ message: "Unable to login. Try again later." });
     }
   };
   const signout = (req, res) => {
@@ -61,13 +57,15 @@ export default function UserRoutes(app) {
     res.json(courses);
   };
   app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
-  app.post("/api/users", createUser);
-  app.get("/api/users", findAllUsers);
-  app.get("/api/users/:userId", findUserById);
+  app.post("/api/users", dao.createUser);
+  app.get("/api/users/:userId", dao.findUserById);
   app.put("/api/users/:userId", updateUser);
-  app.delete("/api/users/:userId", deleteUser);
+  app.delete("/api/users/:userId", dao.deleteUser);
   app.post("/api/users/signup", signup);
-
+  app.get("/api/users", (req, res) => {
+    const users = dao.findAllUsers();
+    res.send(users);
+  });
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
